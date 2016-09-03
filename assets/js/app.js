75e3338b80dfe36779cd1fae9bdb184af3860ec4
Gitaek kwon <break@kimsq.com>
2016-09-03 16:46:16 +0900

    1
    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,2 @@
 // script
 RC_initDrawer();
-
-// bar-header-secondary 상세검색
-$('#moreSearch').on('show.rc.collapse', function () {
-  $('#header-secondary').addClass("hd-expanded")
-})
-$('#moreSearch').on('hide.rc.collapse', function () {
-  $('#header-secondary').removeClass("hd-expanded")
-})
