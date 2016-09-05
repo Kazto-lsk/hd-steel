a5488ce2a56167f400478797fc17b3b1decf567b
Gitaek kwon <break@kimsq.com>
2016-09-06 03:30:18 +0900

    1
    1

--- assets/js/app-tm.js
+++ assets/js/app-tm.js
@@ -1,10 +1,26 @@
 // script
 
 
-
 RC_initDrawer();
-
 
 $('#map-close').tap(function() {
   $('#map').drawer('hide')
 });
+
+$('#map').on('show.rc.drawer', function () {
+  $('.snap-content').css("width","calc(100% - 640px)");
+})
+$('#map').on('hide.rc.drawer', function () {
+  $('.snap-content').css("width","100%");
+})
+
+
+// 임시 구글맵
+
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+      center: {lat: 44.540, lng: -78.546},
+      zoom: 8
+  });
+}
