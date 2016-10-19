91b42bb9dc3524ee65446531fac9f06e1d9a1b41
Gitaek kwon <break@kimsq.com>
2016-10-19 10:53:35 +0900

    1

--- assets/js/app-tm.js
+++ assets/js/app-tm.js
@@ -24,3 +24,43 @@ function initMap() {
       zoom: 10
   });
 }
+
+$('#popup-group-01').on('show.rc.popup',function(){
+  $(this).load('./template/popup-group-01.html',function(){
+    $('#popup-group-01 .table tr').on('tap',function(){
+      var value = $(this).data('label');
+      $('#group-01').val(value);
+      $(this).popup('hide');
+    });
+  })
+});
+
+$('#popup-group-02').on('show.rc.popup',function(){
+  $(this).load('./template/popup-group-02.html',function(){
+    $('#popup-group-02 .table tr').on('tap',function(){
+      var value = $(this).data('label');
+      $('#group-02').val(value);
+      $(this).popup('hide');
+    });
+  })
+});
+
+$('#popup-group-03').on('show.rc.popup',function(){
+  $(this).load('./template/popup-group-03.html',function(){
+    $('#popup-group-03 .table tr').on('tap',function(){
+      var value = $(this).data('label');
+      $('#group-03').val(value);
+      $(this).popup('hide');
+    });
+  })
+});
+
+$('#popup-group-04').on('show.rc.popup',function(){
+  $(this).load('./template/popup-group-04.html',function(){
+    $('#popup-group-04 .table tr').on('tap',function(){
+      var value = $(this).data('label');
+      $('#group-04').val(value);
+      $(this).popup('hide');
+    });
+  })
+});
